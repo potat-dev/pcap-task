cc860b20cd3a42f7aaa91a4b345437fcaf230872
Den <denis100703@gmail.com>
2024-01-28 01:15:30 +0300

    Initialize CMake project with Pcap++ dependency

---
+++ cpp-logger/src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <IPv4Layer.h>
+#include <Packet.h>
+#include <PcapFileDevice.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		std::cout << "Usage: app <file.pcap>" << std::endl;
+		return 0;
+	}
+	// open a pcap file for reading
+	pcpp::PcapFileReaderDevice reader(argv[1]);
+	if (!reader.open())
+	{
+		std::cerr << "Error opening the pcap file" << std::endl;
+		return 1;
+	}
+
+	// read the first (and only) packet from the file
+	pcpp::RawPacket rawPacket;
+	if (!reader.getNextPacket(rawPacket))
+	{
+		std::cerr << "Couldn't read the first packet in the file" << std::endl;
+		return 1;
+	}
+
+	// parse the raw packet into a parsed packet
+	pcpp::Packet parsedPacket(&rawPacket);
+
+	// verify the packet is IPv4
+	if (parsedPacket.isPacketOfType(pcpp::IPv4))
+	{
+		// extract source and dest IPs
+		pcpp::IPv4Address srcIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIPv4Address();
+		pcpp::IPv4Address destIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
+
+		// print source and dest IPs
+		std::cout
+			<< "Source IP is '" << srcIP << "'; "
+			<< "Dest IP is '" << destIP << "'"
+			<< std::endl;
+	}
+
+	// close the file
+	reader.close();
+
+	return 0;
+}
